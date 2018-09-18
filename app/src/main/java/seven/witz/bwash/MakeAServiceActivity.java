daa364622836ba7b7a7f61f0464e272d3be99e70
Administrator <daovanbao96@gmail.com>
2018-09-18 17:16:24 +0700

    saa

---
+++ app/src/main/java/seven/witz/bwash/MakeAServiceActivity.java
@@ -0,0 +1,13 @@
+package seven.witz.bwash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakeAServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_aservice);
+    }
+}
